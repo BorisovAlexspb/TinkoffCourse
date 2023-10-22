290087b43465e11fbbbd67d33ac46597576aaaa7
BorisovAlexspb <146634980+BorisovAlexspb@users.noreply.github.com>
2023-10-23 02:21:00 +0300

    Delete ConnectionException.java

--- src/main/java/edu/hw2/Task3/ConnectionException.java
+++
@@ -1,7 +0,0 @@
-package edu.hw2.Task3;
-
-public class ConnectionException extends RuntimeException {
-    public void getException() {
-        throw new RuntimeException("Connection Exception!");
-    }
-}
