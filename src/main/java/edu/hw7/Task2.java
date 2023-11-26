383e72b6e8a2b9ed5f912f6a591a01ea46a1d5b6
Alexander Borisov <borisov7309@mail.ru>
2023-11-26 23:12:43 +0300

    hw7 (доделать)

---
+++ src/main/java/edu/hw7/Task2.java
@@ -0,0 +1,14 @@
+package edu.hw7;
+
+import java.util.stream.LongStream;
+
+public class Task2 {
+
+    private Task2() {
+    }
+
+    public static Long getFactorial(int n) {
+        return LongStream.range(1, n).parallel().reduce((a, b) -> a * b).getAsLong();
+    }
+
+}
---
+++ src/main/java/edu/hw7/Task3/ThreadedPersonDataBaseSynchronized.java
@@ -0,0 +1,88 @@
+package edu.hw7.Task3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ThreadedPersonDataBaseSynchronized implements PersonDatabase {
+
+    private final Map<Integer, Person> idToPerson = new HashMap<>();
+    private final Map<String, List<Person>> phoneNumberToPerson = new HashMap<>();
+    private final Map<String, List<Person>> nameToPerson = new HashMap<>();
+    private final Map<String, List<Person>> addressToPerson = new HashMap<>();
+
+    @Override
+    public void add(Person person) {
+        synchronized (idToPerson) {
+            if (!idToPerson.containsKey(person.id())) {
+
+                //add phone number
+                if (phoneNumberToPerson.containsKey(person.phoneNumber())) {
+                    phoneNumberToPerson.get(person.phoneNumber()).add(person);
+                } else {
+                    phoneNumberToPerson.put(person.phoneNumber(), new ArrayList<>(List.of(person)));
+                }
+
+                // add name
+                if (nameToPerson.containsKey(person.name())) {
+                    nameToPerson.get(person.name()).add(person);
+                } else {
+                    nameToPerson.put(person.name(), new ArrayList<>(List.of(person)));
+                }
+
+                // add address
+                if (addressToPerson.containsKey(person.address())) {
+                    addressToPerson.get(person.address()).add(person);
+                } else {
+                    addressToPerson.put(person.address(), new ArrayList<>(List.of(person)));
+                }
+
+                // add id - person
+                idToPerson.put(person.id(), person);
+            }
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        //var newId = new AtomicInteger(id);
+        synchronized (idToPerson) {
+            Person person = idToPerson.get(id);
+            if (person != null) {
+                // delete phone number
+                phoneNumberToPerson.get(person.phoneNumber()).removeIf(v -> v.id() == id);
+
+                // delete name
+                nameToPerson.get(person.name()).removeIf(v -> v.id() == id);
+
+                // delete address
+                addressToPerson.get(person.address()).removeIf(v -> v.id() == id);
+
+                // delete id and it's value (person)
+                idToPerson.remove(id);
+            }
+        }
+    }
+
+    @Override
+    public synchronized List<Person> findByName(String name) {
+        synchronized (nameToPerson) {
+            return nameToPerson.get(name);
+        }
+    }
+
+    @Override
+    public synchronized List<Person> findByAddress(String address) {
+        synchronized (addressToPerson) {
+            return addressToPerson.get(address);
+        }
+    }
+
+    @Override
+    public synchronized List<Person> findByPhone(String phone) {
+        synchronized (phoneNumberToPerson) {
+            return phoneNumberToPerson.get(phone);
+        }
+    }
+}
