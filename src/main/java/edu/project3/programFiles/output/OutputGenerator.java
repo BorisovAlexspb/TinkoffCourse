0cf7dc2271ddb901db11aadb874ca3de90b0c868
Alexander Borisov <borisov7309@mail.ru>
2023-12-11 15:20:32 +0300

    delete commit from project3

--- src/main/java/edu/project3/programFiles/output/OutputGenerator.java
+++
@@ -1,9 +0,0 @@
-package edu.project3.programFiles.output;
-
-import java.util.Map;
-
-public interface OutputGenerator {
-    String FILE = "src/main/java/edu/project3/output/";
-
-    <T> void generate(Map<T, T>... maps);
-}
