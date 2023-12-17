d26ba8927890a256cd48fec06566a2f08862ad4d
Alexander Borisov <borisov7309@mail.ru>
2023-12-17 21:39:03 +0300

    delete old commits

--- src/main/java/edu/hw10/task1/generator/fields_generator/FieldsGenerator.java
+++
@@ -1,8 +0,0 @@
-package edu.hw10.task1.generator.fields_generator;
-
-import java.lang.annotation.Annotation;
-
-public interface FieldsGenerator {
-
-    Object generate(Annotation[] annotations);
-}
